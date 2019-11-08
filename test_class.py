3a24b7a3279710b60b2a5b25a6f9fd787828d0b7
bellahOchola <bellahkenya@gmail.com>
2019-11-08 14:45:30 +0300

    add docstring to the class created in the test file

--- test_class.py
+++ test_class.py
@@ -1,2 +1,10 @@
 import unittest
 from password import User
+
+class TestUser(unittest.Testcase):
+    '''
+    This is a test class that defines test cases 
+for user login and signup details.
+    Args:
+        unittest.TestCase: TestCase class aids in the formation of test cases
+    '''
