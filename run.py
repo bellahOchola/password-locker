7be54cdf5ac573c22b08637a965a752f3726bbc7
bellahOchola <bellahkenya@gmail.com>
2019-11-10 22:17:07 +0300

    add style to the app"password locker"

--- run.py
+++ run.py
@@ -53,20 +53,28 @@ def find_account(account_name):
     return Credentials.find_by_accountname(account_name)
 
 def main():
+    print('####     #     ####   ####   #     #   ###   # ###    # ###          #        ###     ####  #      #   ####   ###          ')
+    print('#   #   # #   #      #       #     #  #   #  #     #  #     #        #       #   #   #      #     #   #      #    #        ')
+    print('#   #  #   #  #      #       #     # #     # #      # #      #       #      #     #  #      #    #    #      #     #       ')
+    print('# #   #  #  #  ####   ####   #  #  # #     # #  #  #  #       #      #      #     #  #      #  #      # ###  #  # #        ')
+    print('#    #      #      #      #  #  #  # #     # #   #    #       #      #      #     #  #      #    #    #      #   #         ')
+    print('#    #      #      #      #  # # # #  #   #  #     #  #      #       #       #   #   #      #      #  #      #     #       ')
+    print('#     #    #   ####   ####   #     #   ###   #       #  ####         #####    ###     ####  #       #  ####  #       #     ')
     print('Heey!!! Welcome to your Password Manager.')
-    # print('\n')
     print('To continue enter one of the following.')
-    
-    print('ca ---- create new account and lo ---login')
+    print('-' * 100)
+    print('CA to Create an Account and LO to Login')
     ans = input().lower()
     if ans == 'ca':
         print('Create an Account')
         print('Username:')
-        username = input()
+        user_name = input()
         print('password:')
         password = input()
         save_account(create_account(user_name,password))
         print(f'Hello {user_name} your account has been created and your password is {password}')
+        print('*' * 100)
+
 
     elif ans == 'lo':
         print('Enter your username')
@@ -76,8 +84,10 @@ def main():
         login = find_user(user_name,password)
         if find_user == login:
             print(f'Heey {user_name} , Welcome to your locker account')
+            print('*' * 100)
+
     while True:
-        print("Use these short codes : cc - create a new credential, vc - view credentials, fc -find a credential, dc -delete a credential, ex -exit the credential list ")
+        print("Use these short codes :\n cc - create a new credential,\n vc - view credentials, \n fc -find a credential,\n dc -delete a credential,\n ex -exit the credential list \n")
         short_code = input().lower()
         if short_code == 'cc':
             print('New Credential Account')
@@ -89,13 +99,15 @@ def main():
             print('\n')
             print(f'Your {account_name} account has been successfully created')
             save_credentials(create_credentials(account_name,password))
+            print('-' * 100)
 
         elif short_code == 'vc':
             if display_credentials():
                 print('Here is a list of your credentials')
-                print('\n')
                 for credential in display_credentials():
-                    print(f'{credential.account_name} ..... {credential.password}')
+                    print(f'Account name: {credential.account_name} ..... Password: {credential.password}')
+                    print('-' * 100)
+
 
             else:
                 print('\n')
@@ -107,6 +119,8 @@ def main():
             if find_account(search_account):
                 search_credential = find_account(search_account)
                 print(f'Your {search_account} account  password is: {search_credential.password}')
+                print('-' * 100)
+
 
             else:
                 print('That account does not exist.')
@@ -118,20 +132,15 @@ def main():
                 search = find_account(name)
                 search.delete_credential()
                 print(f'Your {search.account_name} has been deleted')
+                print('-' * 100)
+
             else:
                 print('That account does not exist')
 
         elif short_code == 'ex':
             print('See you soon........')
-        break        
+            break   
 
-
-
-    
-
-
-
-    
 
 if __name__ == '__main__':
     main()
